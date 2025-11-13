dfc61aa0ddd27e4b14b8c934c05274fb8a8d74f2
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-12 19:37:03 -0500

    added cmdline option for # of workers

--- API/start.py
+++ API/start.py
@@ -1,15 +1,22 @@
 from __init__ import this
-from philh_myftp_biz import run
+from philh_myftp_biz import run, Args
 from philh_myftp_biz.pc import script_dir
 
 script_dir(__file__).cd()
+
+args = Args()
+
+if len(args) == 1:
+    workers = args[0]
+else:
+    workers = 1
 
 API = run(
     args = [
         'uvicorn',
         'app:app',
         '--host', '0.0.0.0',
-        '--workers', 1,
+        '--workers', workers,
         '--ssl-certfile', this.file('certificates', 'cert').path,
         '--ssl-keyfile', this.file('certificates', 'key').path
     ],
