4ff78ae69a0364078b9f40fa2159b436737d45e5
Phil Hunt <philiphuntiv2@gmail.com>
2025-12-20 19:17:33 -0500

    Update __Start.py

--- Indexer/__Start.py
+++ Indexer/__Start.py
@@ -5,25 +5,25 @@ PIDstore.save(getpid())
 
 registries: list[IndexRegistry] = []
 
-#
+# Append the root registry
+registries += [IndexRegistry(root)]
+
+# Iter through all subfolders of root
 for p in root.descendants():
-
-    #
     if p.isdir():
-
-        #
+        # Append the folder as a registry
         registries += [IndexRegistry(p)]
 
-#
+# Iter through the registries
 for r in registries:
 
-    #
+    # Iter through all items in the registry
     for i in r.items():
         
-        #
+        # Update the registry item
         i.Update()
 
     print('Building Registry:', r.dir)
 
-    #
+    # Build the registry
     r.build()
