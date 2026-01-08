5ab093b3082b6539997850b4a09746565f04815d
Phil Hunt <philiphuntiv2@gmail.com>
2026-01-08 13:21:20 -0500

    Improved Human Feedback

--- Root/Server/Plex/Download/script.js
+++ Root/Server/Plex/Download/script.js
@@ -1,15 +1,35 @@
 
 e['options'] = document.getElementById('options')
-e['Search'] = document.getElementById('Search')
+e['search'] = document.getElementById('Search')
 
 // ============================================================================================
 
 function getItem(Type, Title, Year) {
 
+    // Save the current html of the search results
+    let oldHTML = e.options.innerHTML
+
+    //
+    e.search.setAttribute('readonly', 'true')
+
+    // Add loading dots
+	e.options.innerHTML = `
+		<div class="loader">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	`
+
+    // Parse the media title
     Title = Title.replace('&', 'and')
     Title = Title.replace(':', '')
     Title = encodeURIComponent(Title)
 
+    // Call the API
     let call = API.call(
 
         url = '/Server/Plex/download',
@@ -22,34 +42,54 @@ function getItem(Type, Title, Year) {
         
     )
 
-    call.then(t => alert(t))
+    // Handle the API response
+    call.then(t => {
+        
+        // Show an alert with the response message
+        alert(t)
+
+        // Restore the saved search results html
+        e.options.innerHTML = oldHTML
+
+        //
+        e.search.removeAttribute('readonly')
+    
+    })
 
 }
 
 // ============================================================================================
 
+// Previous Search Term
 let lterm = ''
 
+// Loop every 100 ms
 setInterval(() => {
 
-	let term = e.Search.value.toLowerCase()
+	// Current Search Term
+    let term = e.search.value.toLowerCase()
 
-	if (term != lterm) {
+    // If Search Term has changed
+    if (term != lterm) {
 
+        // Save the Current Term as the Previous Term
         lterm = term
 
         // Clear all options
         e.options.innerHTML = ''
 
-        //
+        // Call 'omdbapi.com'
         fetch(`https://www.omdbapi.com/?apikey=97f79170&s=${term}`).then(r => r.json()).then(t => {
 
+            // If a response is given
             if (t.Response == 'True') {
 
+                // Iter through search results
                 for (x in t.Search) {
 
                     let i = t.Search[x]
 
+                    // Parse the media release year
                     i.Year = i.Year.substring(0,4)
                     
                     // Insert element with item details
@@ -57,7 +97,6 @@ setInterval(() => {
                         <img 
                             src     = "${i.Poster}"
                             title   = "${i.Title} (${i.Year})"
-                            width   = 300
                             onclick = "getItem('${i.Type}', '${i.Title}', ${i.Year})"
                             onerror = "this.remove()"
                         >
@@ -65,10 +104,16 @@ setInterval(() => {
                     
                 }
 
+            // If no response is given
             } else {
             
-                e.options.innerHTML = `<h1>No Results for</h1> <br> <h1 style="line-break: anywhere;">${term}</h1>`
-            
+                // Add a 'no results' message
+                e.options.innerHTML = `
+                    <h1>No Results for</h1>
+                    <br>
+                    <h1 style="line-break: anywhere;">${term}</h1>
+                `
+
             }
 
         })
