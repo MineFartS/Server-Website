5ee22e42674724ab3fea941f4ed00c11ad18d7fd
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-03 20:20:31 -0400

    Allow Only 1 ToDo List

--- Root/Apps/ToDo/script.js
+++ Root/Apps/ToDo/script.js
@@ -1,2 +1,36 @@
 
 e['boxes'] = document.getElementById('boxes')
+
+data = {
+
+    'read': (e) => {
+
+        API.auth('/Apps/Bookmark/read', {x:e.id+3})
+            .then(t => e.value = t)
+
+    },
+
+    'save': (e) => {
+
+        API.auth(
+            '/Apps/Bookmark/save',
+            {
+                'x': e.id+3,
+                'value': e.value
+            }
+        )
+
+    },
+
+    'clear': (e) => {
+
+        e.value = ''
+
+        data.save(e)
+
+        e.destroy()
+
+    }
+
+
+}
