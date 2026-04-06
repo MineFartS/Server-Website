9c11adb5a5b42adaf74ce5c12237e29e90243708
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-05 22:09:32 -0400

    Remove Junctions.py

--- Indexer/__Start.py
+++ Indexer/__Start.py
@@ -1,9 +1,6 @@
 
-# Run Junctions.py
-from . import Junctions # pyright: ignore[reportUnusedImport]
-
 # Run Registry.py
 from . import Registry # pyright: ignore[reportUnusedImport]
 
 # Run IISConfig.py
-from . import IISConfig # pyright: ignore[reportUnusedImport]
+import IISConfig # pyright: ignore[reportUnusedImport]
