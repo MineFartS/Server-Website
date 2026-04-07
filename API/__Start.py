b07b4d6255e2ae16fd4a1d707cf4e42e84f7ed78
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-06 20:55:59 -0400

    Use fastapi fork

--- API/__Start.py
+++ API/__Start.py
@@ -1,19 +1,53 @@
 from philh_myftp_biz.web import FirewallException
+from philh_myftp_biz.terminal import Log
 from philh_myftp_biz import VERBOSE
+from importlib import import_module
 from . import this, PIDstore
-from uvicorn import run
+from fastapi import FastAPI
 from os import getpid
+
+#=======================================================================
 
 FirewallException('Uvicorn').set(8000)
 
 PIDstore.save([getpid()])
 
-run(
-    app = 'API.app:app',
-    host = '0.0.0.0',
-    port = 8000,
-    workers = (None if VERBOSE else 2),
-    ssl_certfile = this.file('certificates/cert').path,
-    ssl_keyfile = this.file('certificates/key').path,
+#=======================================================================
+# APP
+
+app = FastAPI()
+
+app.set_favicon(this.child('/Root/_/main.ico'))
+
+#=======================================================================
+# ROUTERS
+
+for file in this.child('/API/Routers/').descendants:
+
+    if (file.ext == 'py') and (file.name != '__init__'):
+
+        imp: str = file.path
+        imp = imp.split('/API/')[1]
+        imp = imp.split('.')[0]
+        imp = imp.replace('/', '.')
+        imp = '.' + imp
+
+        Log.VERB(f'Installing Router: {imp}')
+
+        module = import_module(
+            name = imp, 
+            package = __package__
+        )
+
+        app.include_router(module.router)
+
+#=======================================================================
+
+Log.INFO('Uvicorn Service Started')
+
+app.run(
+    workers = (1 if VERBOSE else 2),
+    ssl_certfile = this.file('certificates/cert'),
+    ssl_keyfile = this.file('certificates/key'),
     log_level = "critical"
 )
